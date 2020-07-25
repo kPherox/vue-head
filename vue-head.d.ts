0c7185a6ac6d1515ae7a5c5e7df336862a2c8a05
kPherox <admin@mail.kr-kp.com>
2020-07-25 12:26:19 +0900

    Change to separated interface
    Add base property

--- vue-head.d.ts
+++ vue-head.d.ts
@@ -7,54 +7,6 @@ interface TitleOptions {
   complement?: string;
 }
 interface ElementOptions extends Object {
-  /** Shorthand of `charset` */
-  ch?: string;
-  charset?: string;
-  /** Shorthand of `target` */
-  tg?: string;
-  target?: string;
-  /** Shorthand of `name` */
-  n?: string;
-  name?: string;
-  /** Shorthand of `http-equiv` */
-  he?: string;
-  'http-equiv'?: string;
-  /** Shorthand of `itemprop` */
-  ip?: string;
-  itemprop?: string;
-  /** Shorthand of `content` */
-  c?: string;
-  content?: string;
-  /** Shorthand of `property` */
-  p?: string;
-  property?: string;
-  /** Shorthand of `scheme` */
-  sc?: string;
-  scheme?: string;
-  /** Shorthand of `rel` */
-  r?: string;
-  rel?: string;
-  /** Shorthand of `href` */
-  h?: string;
-  href?: string;
-  /** Shorthand of `sizes` */
-  sz?: string;
-  sizes?: string;
-  /** Shorthand of `type` */
-  t?: string;
-  type?: string;
-  /** Shorthand of `src` */
-  s?: string;
-  src?: string;
-  /** Shorthand of `async` */
-  a?: boolean;
-  async?: boolean;
-  /** Shorthand of `defer` */
-  d?: string;
-  defer?: string;
-  /** Shorthand of `inner` */
-  i?: string;
-  inner?: string;
   /**
    * Option for undo tag when leave the component
    * https://github.com/ktquez/vue-head#undo-elements
@@ -70,14 +22,82 @@ interface ElementOptions extends Object {
    */
   body?: boolean;
 }
+interface MetaOptions extends ElementOptions {
+  /** Shorthand of `charset` */
+  ch?: string;
+  charset?: string;
+  /** Shorthand of `name` */
+  n?: string;
+  name?: string;
+  /** Shorthand of `http-equiv` */
+  he?: string;
+  'http-equiv'?: string;
+  /** Shorthand of `itemprop` */
+  ip?: string;
+  itemprop?: string;
+  /** Shorthand of `content` */
+  c?: string;
+  content?: string;
+  /** Shorthand of `property` */
+  p?: string;
+  property?: string;
+}
+interface BaseOptions extends ElementOptions {
+  /** Shorthand of `target` */
+  tg?: string;
+  target?: string;
+}
+interface LinkOptions extends ElementOptions {
+  /** Shorthand of `rel` */
+  r?: string;
+  rel?: string;
+  /** Shorthand of `href` */
+  h?: string;
+  href?: string;
+  /** Shorthand of `sizes` */
+  sz?: string;
+  sizes?: string;
+  /** Shorthand of `type` */
+  t?: string;
+  type?: string;
+}
+interface ScriptOptions extends ElementOptions {
+  /** Shorthand of `type` */
+  t?: string;
+  type?: string;
+  /** Shorthand of `scheme` */
+  sc?: string;
+  scheme?: string;
+  /** Shorthand of `src` */
+  s?: string;
+  src?: string;
+  /** Shorthand of `async` */
+  a?: boolean;
+  async?: boolean;
+  /** Shorthand of `defer` */
+  d?: string;
+  defer?: string;
+  /** Shorthand of `inner` */
+  i?: string;
+  inner?: string;
+}
+interface StyleOptions extends ElementOptions {
+  /** Shorthand of `type` */
+  t?: string;
+  type?: string;
+  /** Shorthand of `inner` */
+  i?: string;
+  inner?: string;
+}
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     head?: {
       title?: TitleOptions | (() => TitleOptions);
-      meta?: ElementOptions[] | (() => ElementOptions[]);
-      link?: ElementOptions[] | (() => ElementOptions[]);
-      script?: ElementOptions[] | (() => ElementOptions[]);
-      style?: ElementOptions[] | (() => ElementOptions[]);
+      meta?: MetaOptions[] | (() => MetaOptions[]);
+      base?: BaseOptions[] | (() => BaseOptions[]);
+      link?: LinkOptions[] | (() => LinkOptions[]);
+      script?: ScriptOptions[] | (() => ScriptOptions[]);
+      style?: StyleOptions[] | (() => StyleOptions[]);
     };
   }
 }
